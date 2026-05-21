a8b7d91dcea6b0497b91e503f165908f89d9f7a3
Kairamkonda Chaithanya <kairamkondachaithanya9@gmail.com>
2026-05-22 04:08:05 +0530

    Disable eslint rule for unused variables

--- client/src/pages/CreateQuizText.jsx
+++ client/src/pages/CreateQuizText.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect, useCallback } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import api from '../utils/api';
