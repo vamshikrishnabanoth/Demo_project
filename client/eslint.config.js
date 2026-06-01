3e8c1b72a0ca84bd460108d7f14fad650bec29db
Kairamkonda Chaithanya <kairamkondachaithanya9@gmail.com>
2026-06-01 11:09:22 +0530

    Modify ESLint config to ignore additional file and rules
    Updated ESLint configuration to ignore 'socket_listener_to_add.js' and disabled several React Hooks rules.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'socket_listener_to_add.js']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -23,7 +23,17 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^([A-Z_]|motion|motionDiv)' }],
+      'no-unused-vars': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'react-hooks/purity': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/refs': 'off',
+      'react-hooks/unsupported-syntax': 'off',
+      'no-useless-escape': 'off',
+      'no-empty': 'off',
+      'react-refresh/only-export-components': 'off'
     },
   },
 ])
+
+
